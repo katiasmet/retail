a1ba53a77dc9f5bb436caca22ba631e6e1a7098f
katiasmet <katia.smet@hotmail.com>
2016-06-10 10:20:05 +0200

    final

--- routes/tweets.php
+++ routes/tweets.php
@@ -20,11 +20,21 @@ $app->get($base, function($request, $response, $args){
   $query = $request->getQueryParams();
 
   $connection = getConnectionWithAccessToken("1608412375-uhvmddZ0CcIo5r3iiH4BUQBMh2S9OUfW6aNYWHb",  "2m2AhbyqM1IaVM8ZkRokZoo14NG4rvY6UQTCO25LY7LH4");
-  
+
+  if(empty($query)) {
+    $data = $response->withStatus(400);
+    return $response;
+  }
+
   if(!empty($query['screen_name'])) { //from specific user
     $data = $connection->get("statuses/user_timeline", ["screen_name" => $query['screen_name'], "count" => $query['count'], "exclude_replies" => true]);
   } else {
     $data = $connection->get("search/tweets", ["q" => $query['search'], "count" => $query['count'], "result_type" => $query['result_type']]);
+  }
+
+  if(empty($data)){
+    $data = $response->withStatus(400);
+    return $response;
   }
 
   $response->getBody()->write(json_encode($data));
